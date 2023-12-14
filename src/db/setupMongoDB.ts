b0250aca6f019f1bcac23465b115fb333b106651
Mhttt <74661052+Mhttt@users.noreply.github.com>
2023-12-14 11:44:42 +0100

    remove comment

--- src/db/setupMongoDB.ts
+++ src/db/setupMongoDB.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-// export default await mongoose.connect(process.env.DB_CONNECTION_STRING!);
+export default await mongoose.connect(process.env.DB_CONNECTION_STRING!);
 
 console.log('-> Connected to Database!');
